0dad2728cb3a1c430865b35e3963057ffa3b0647
javohirfrontenddevs <javohirabdusharipov676@gmail.com>
2025-04-11 23:38:12 +0500

    added online_hr project

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,25 @@
+Abdusharipov_Javohir_resume;
+
 import React from "react";
 import "./Hero.css";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function Hero() {
+  const handleDownloadResume = () => {
+    const resumeUrl =
+      "https://my-portfolio-website-six-xi.vercel.app/" +
+      "/Javohir_Abdusharipov_Resume.docx";
+
+    const link = document.createElement("a");
+    link.href = resumeUrl;
+    link.download = "Javohir_Abdusharipov_Resume.docx";
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+  };
+
   return (
     <div id="home" className="hero">
       <img src="./myPicture.jpg" alt="Javohir Abdusharipov" />
@@ -12,7 +29,9 @@ function Hero() {
         in Node.js and React.js.
       </p>
       <div className="hero_action">
-        <div className="hero_resume">My resume</div>
+        <div className="hero_resume" onClick={handleDownloadResume}>
+          My resume
+        </div>
       </div>
     </div>
   );
